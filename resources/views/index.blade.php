bdb1058221cddc8138ee7d506e86c3ede7ae0e3b
hrshishym <chappy.dogheart@gmail.com>
2024-11-27 16:53:47 +0900

    一覧画面を作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,33 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-  <title>Document</title>
-  @vite('resources/css/app.css')
-  @livewireStyles
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+    @livewireStyles
 </head>
 <body class="p-5">
-  <h1>カウントアップ</h1>
-  <livewire:counter-sample />
-  @if($id != null)
-  <hr>
-  <h1>トグル</h1>
-  <livewire:star-component :id="$id" />
-@endif
-@livewireScripts
+    <h1>カウントアップ</h1>
+    <livewire:counter-sample />
+    @if($id != null)
+        <hr>
+        <h1>トグル</h1>
+        <livewire:star-component :id="$id" />
+        <hr>
+        <a href="/">戻る</a>
+    @else
+    <!-- 全てのStar->idへのリンクを作成 -->
+    @php
+        $stars = App\Models\Star::all();
+
+        foreach($stars as $star) {
+            echo '<a href="/' . $star->id . '">ID=' . $star->id . '</a> <br>';
+        }
+
+    @endphp
+    @endif
+    @livewireScripts
 </body>
 </html>
